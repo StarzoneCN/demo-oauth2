8f9012cefa78495a0b4f0ef9afd2015a0901509b
Li Hongxing <lihongxing@bluemoon.com.cn>
2017-11-22 14:37:52 +0800

    配置JdbcClientService实现client信息持久化

--- src/main/java/com/example/demooauth2/config/CustomAuthorizationServerConfigurerAdapter.java
+++ src/main/java/com/example/demooauth2/config/CustomAuthorizationServerConfigurerAdapter.java
@@ -1,19 +1,57 @@
 package com.example.demooauth2.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+import javax.sql.DataSource;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class CustomAuthorizationServerConfigurerAdapter extends AuthorizationServerConfigurerAdapter {
 
+    @SuppressWarnings("SpringJavaAutowiringInspection")
+    @Autowired
+    private DataSource dataSource;
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Bean
+    public TokenStore tokenStore(){
+        return new JdbcTokenStore(dataSource);
+    }
+
+    @Bean
+    public ClientDetailsService clientDetailsService(){
+        return new JdbcClientDetailsService(dataSource);
+    }
+
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients
-                .inMemory()
-                    .withClient("client")
-                    .secret("secret")
-                    .authorizedGrantTypes("authorization_code")
-                    .scopes("app");
+        clients.withClientDetails(new JdbcClientDetailsService(dataSource));
+    }
+
+    @Override // 配置框架应用上述实现
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager);
+        endpoints.tokenStore(tokenStore());
+
+        // 配置TokenServices参数
+        DefaultTokenServices tokenServices = new DefaultTokenServices();
+        tokenServices.setTokenStore(endpoints.getTokenStore());
+        tokenServices.setSupportRefreshToken(true);
+        tokenServices.setClientDetailsService(endpoints.getClientDetailsService());
+        tokenServices.setTokenEnhancer(endpoints.getTokenEnhancer());
+        tokenServices.setAccessTokenValiditySeconds( (int) TimeUnit.DAYS.toSeconds(30)); // 30天
+        endpoints.tokenServices(tokenServices);
     }
 }
