99b79487b05866e0b7e759bb84063b875d469d5a
LiHongxing <lihongxing@bluemoon.com.cn>
2019-01-04 09:59:58 +0800

    no message

--- src/main/java/com/example/demooauth2/mybatisPlus/service/CustomUserDetailsService.java
+++
@@ -1,7 +0,0 @@
-package com.example.demooauth2.mybatisPlus.service;
-
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface CustomUserDetailsService extends UserDetailsService {
-
-}
