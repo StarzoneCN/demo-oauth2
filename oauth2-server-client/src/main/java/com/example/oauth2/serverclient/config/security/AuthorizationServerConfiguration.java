8811f464dedafedad0858d5b14ee6d0d4246229c
LiHongxing <lihongxing@bluemoon.com.cn>
2019-01-08 14:48:56 +0800

    数据库存储client信息

--- oauth2-server-client/src/main/java/com/example/oauth2/serverclient/config/security/AuthorizationServerConfiguration.java
+++ oauth2-server-client/src/main/java/com/example/oauth2/serverclient/config/security/AuthorizationServerConfiguration.java
@@ -1,10 +1,17 @@
 package com.example.oauth2.serverclient.config.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
 
 /**
  * 授权认证配置类
@@ -18,13 +25,24 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 @EnableAuthorizationServer
 public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
 
+    @Resource
+    private DataSource dataSource;
+
+    @Bean
+    @Order(1)
+    public JdbcClientDetailsService jdbcClientDetailsService(){
+        return new JdbcClientDetailsService(dataSource);
+    }
+
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.inMemory() // 使用in-memory存储
+        /*clients.inMemory() // 使用in-memory存储
                 .withClient("test_oauth_client") // client_id
                     .secret("$2a$10$afwGl2b.fIJKd/8ekyP0gemzU/dhn/9QX.MPle3Xp63sUjzHhLy1a") // BCryptPasswordEncoder加密
                     .authorizedGrantTypes("authorization_code") // 该client允许的授权类型
-                    .scopes("app"); // 允许的授权范围
+                    .scopes("app"); // 允许的授权范围*/
+
+        clients.withClientDetails(jdbcClientDetailsService());
     }
 
     @Override
