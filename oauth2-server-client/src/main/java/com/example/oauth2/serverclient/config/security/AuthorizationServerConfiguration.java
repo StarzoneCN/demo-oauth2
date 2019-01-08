2448fe5da857821d140414fd981fac756c754a11
LiHongxing <lihongxing@bluemoon.com.cn>
2019-01-08 13:06:45 +0800

    no message

--- oauth2-server-client/src/main/java/com/example/oauth2/serverclient/config/security/AuthorizationServerConfiguration.java
+++ oauth2-server-client/src/main/java/com/example/oauth2/serverclient/config/security/AuthorizationServerConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
 /**
  * 授权认证配置类
@@ -13,14 +14,23 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
  * @date: Create in 2019/1/4 10:06
  * @modefied:
  */
+@Configuration
+@EnableAuthorizationServer
 public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory() // 使用in-memory存储
                 .withClient("client") // client_id
-                .secret("secret") // client_secret
-                .authorizedGrantTypes("authorization_code") // 该client允许的授权类型
-                .scopes("app"); // 允许的授权范围
+                    .secret("secret") // client_secret
+                    .authorizedGrantTypes("authorization_code") // 该client允许的授权类型
+                    .scopes("app"); // 允许的授权范围
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+        //enable client to get the authenticated when using the /oauth/token to get a access token
+        //there is a 401 authentication is required if it doesn't allow form authentication for clients when access /oauth/token
+        oauthServer.allowFormAuthenticationForClients();
     }
 }
--- oauth2-server-client/src/main/java/com/example/oauth2/serverclient/config/security/WebSecurityConfig.java
+++ oauth2-server-client/src/main/java/com/example/oauth2/serverclient/config/security/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.oauth2.serverclient.config.security;
 import com.example.oauth2.serverclient.service.impl.CustomUserDetialsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationEventPublisher;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -82,7 +83,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/login").permitAll()
+                .antMatchers("oauth/authorize", "oauth/token",  "/login").permitAll()
                     .anyRequest().authenticated().and()
                 .formLogin().permitAll().and()
                 .rememberMe()
