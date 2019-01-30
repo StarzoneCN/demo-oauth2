1d1eacd8c4ec2f1772eea1c9d769e83eafc793df
LiHongxing <lihongxing@bluemoon.com.cn>
2019-01-30 16:22:37 +0800

    解决跨域问题

--- oauth2-server-resource/src/main/java/personal/starzonecn/example/oauth2/resource/config/filters/CorsFilter.java
+++ oauth2-server-resource/src/main/java/personal/starzonecn/example/oauth2/resource/config/filters/CorsFilter.java
@@ -1,9 +1,12 @@
 package personal.starzonecn.example.oauth2.resource.config.filters;
 
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
@@ -17,9 +20,10 @@ import java.io.IOException;
  */
 @Component
 @WebFilter(filterName = "corsFilter", urlPatterns = "/*")
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
 
-    @Override
+/*    @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse httpServletResponse = (HttpServletResponse) response;
         httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
@@ -28,5 +32,23 @@ public class CorsFilter implements Filter {
         httpServletResponse.setHeader("Access-Control-Allow-Headers", "x-requested-with,Authorization");
         httpServletResponse.setHeader("Access-Control-Allow-Credentials", "true");
         chain.doFilter(request, httpServletResponse);
+    }*/
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse resp,
+                         FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) resp;
+        HttpServletRequest request = (HttpServletRequest) req;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN");
+
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            chain.doFilter(req, resp);
+        }
+
     }
 }
